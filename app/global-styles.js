4a8db8d601d3e3cf90baf2a2e581e476d855629c
David Fischer <fischer@sipgate.de>
2017-03-30 09:19:33 +0200

    add headline sizes

--- app/global-styles.js
+++ app/global-styles.js
@@ -3,7 +3,7 @@ import { injectGlobal } from 'styled-components';
 /* eslint no-unused-expressions: 0 */
 injectGlobal`
   html {
-    font-size: 100%;
+    font-size: 1rem;
     box-sizing: border-box;
   }
 
@@ -19,15 +19,42 @@ injectGlobal`
 
   body {
     font-family: 'Helvetica Neue', Helvetica, Arial, sans-serif;
+    line-height: 1.45;
+    color: #333;
   }
 
   body.fontLoaded {
     font-family: 'Roboto', 'Helvetica Neue', Helvetica, Arial, sans-serif;
   }
 
-  #app {
-    background-color: #fafafa;
-    min-height: 100%;
-    min-width: 100%;
+  p {
+    margin-bottom: 1.3em;
+  }
+
+  h1, h2, h3, h4 {
+    margin: 1.414em 0 0.5em;
+    font-weight: inherit;
+    line-height: 1.2;
+  }
+
+  h1 {
+    margin-top: 0;
+    font-size: 2.441em;
+  }
+
+  h2 {
+    font-size: 1.953em;
+  }
+
+  h3 {
+    font-size: 1.563em;
+  }
+
+  h4 {
+    font-size: 1.25em;
+  }
+
+  small, .font_small {
+    font-size: 0.8em;
   }
 `;
