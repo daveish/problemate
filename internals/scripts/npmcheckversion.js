e54d37fc3ebcfb79911ab6b99c32800c107b8c77
David Fischer <da.e.fischer@googlemail.com>
2017-04-23 11:59:27 +0200

    raise needed npm version to 4

--- internals/scripts/npmcheckversion.js
+++ internals/scripts/npmcheckversion.js
@@ -1,8 +1,8 @@
 const exec = require('child_process').exec;
 exec('npm -v', function (err, stdout, stderr) {
   if (err) throw err;
-  if (parseFloat(stdout) < 3) {
-    throw new Error('[ERROR: React Boilerplate] You need npm version @>=3');
+  if (parseFloat(stdout) < 4) {
+    throw new Error('[ERROR: React Boilerplate] You need npm version @>=4');
     process.exit(1);
   }
 });
