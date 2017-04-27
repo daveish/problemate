9a61ba7c44f0e7d5fd7ec2347d0aa8f9e827ac24
David Fischer <da.e.fischer@googlemail.com>
2017-04-27 20:03:31 +0200

    remove console log

--- app/containers/AppDrawerToggle/reducer.js
+++ app/containers/AppDrawerToggle/reducer.js
@@ -14,10 +14,8 @@ const initialState = fromJS({
 function appDrawerToggleReducer(state = initialState, action) {
   switch (action.type) {
     case TOGGLE_APP_DRAWER:
-      console.log(action.type);
       return state.set('appDrawerOpen', !state.get('appDrawerOpen'));
     default:
-      console.log(action.type);
       return state;
   }
 }
