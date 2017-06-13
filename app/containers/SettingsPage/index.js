3ddff2cbf5dbe86dfb918659133d01f1234d82ab
David Fischer <da.e.fischer@googlemail.com>
2017-06-13 09:01:59 +0200

    add settings component

---
+++ app/containers/SettingsPage/index.js
@@ -0,0 +1,34 @@
+/*
+ *
+ * SettingsPage
+ *
+ */
+
+import React from 'react';
+import PropTypes from 'prop-types';
+import { connect } from 'react-redux';
+import { FormattedMessage } from 'react-intl';
+import messages from './messages';
+
+export class SettingsPage extends React.PureComponent { // eslint-disable-line react/prefer-stateless-function
+  render() {
+    return (
+      <div>
+        <FormattedMessage {...messages.header} />
+      </div>
+    );
+  }
+}
+
+SettingsPage.propTypes = {
+  dispatch: PropTypes.func.isRequired,
+};
+
+
+function mapDispatchToProps(dispatch) {
+  return {
+    dispatch,
+  };
+}
+
+export default connect(null, mapDispatchToProps)(SettingsPage);
