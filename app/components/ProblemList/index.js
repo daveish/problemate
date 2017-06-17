cca4a02cf498e104b70a788fd7b1126e761357c9
David Fischer <da.e.fischer@googlemail.com>
2017-06-17 18:18:37 +0200

    add dummy images for problem cards

--- app/components/ProblemList/index.js
+++ app/components/ProblemList/index.js
@@ -6,19 +6,21 @@
 
 import React from 'react';
 import ProblemCard from '../ProblemCard/index';
+import CardImage from './300x100.jpg';
+import AvatarImage from './40x40.jpg';
 
 const cardItems = [];
 
 for (let i = 0; i < 10; i += 1) {
   cardItems.push({
     id: i,
-    image: 'http://lorempixel.com/300/100/',
+    image: CardImage,
     imageAlternative: '',
     title: 'Device works but does not update',
     author: 'RubyCat',
     upvotes: 100,
     created: 'vor 2 Minuten',
-    avatar: 'http://lorempixel.com/40/40/people/',
+    avatar: AvatarImage,
   });
 }
 
