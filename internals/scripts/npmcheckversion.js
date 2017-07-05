baf64ce933f069f949ea551cd1115ef38cfb2fe4
David Fischer <da.e.fischer@googlemail.com>
2017-07-05 08:41:25 +0200

    format npmcheckversion script

--- internals/scripts/npmcheckversion.js
+++ internals/scripts/npmcheckversion.js
@@ -1,8 +1,11 @@
 const exec = require('child_process').exec;
-exec('npm -v', function (err, stdout, stderr) {
-  if (err) throw err;
+
+exec('npm -v', function (err, stdout) {
+  if (err) {
+    throw err;
+  }
+
   if (parseFloat(stdout) < 4) {
     throw new Error('[ERROR: React Boilerplate] You need npm version @>=4');
-    process.exit(1);
   }
 });
