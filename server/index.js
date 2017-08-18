3e9377e4c9e74e7728ca2b8f5772a00426b188cf
daveish <da.e.fischer@googlemail.com>
2017-08-19 01:40:00 +0200

    add heroku specific header for ssl with express

--- server/index.js
+++ server/index.js
@@ -28,7 +28,7 @@ const prettyHost = customHost || 'localhost';
 
 // Start your app.
 app.listen(port, host, (err) => {
-  app.use(enforce.HTTPS());
+  app.use(enforce.HTTPS({ trustProtoHeader: true }));
 
   if (err) {
     return logger.error(err.message);
