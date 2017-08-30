227cae50fc2e6d1d3ac814864b2099ea36c82a2c
daveish <da.e.fischer@googlemail.com>
2017-08-30 20:17:32 +0200

    add link to ProblemCard

--- app/components/ProblemCard/index.js
+++ app/components/ProblemCard/index.js
@@ -11,7 +11,12 @@ import CardMedia from 'material-ui/Card/CardMedia';
 import Typography from 'material-ui/Typography/Typography';
 import PropTypes from 'prop-types';
 import React from 'react';
+import { Link } from 'react-router-dom';
 import styled from 'styled-components';
+
+const StyledLink = styled(Link)`
+  text-decoration: none;
+`;
 
 const StyledCard = styled(Card)`
   margin: 0.625rem;
@@ -56,33 +61,36 @@ class ProblemCard extends React.PureComponent { // eslint-disable-line react/pre
     const bullet = <Bullet>•</Bullet>;
 
     return (
-      <StyledCard>
-        <CardMediaWrapper>
-          <StyledCardMedia image={this.props.image} title={this.props.imageAlternative} />
-        </CardMediaWrapper>
-        <CardHeader
-          title={<Typography>{this.props.title}</Typography>}
-          subheader={
-            <StyledCardSubtitle>
-              <Typography type="caption">
-                <StyledListItem>{this.props.author}</StyledListItem>
-                {bullet}
-                <StyledListItem>{this.props.upvotes} Feel you</StyledListItem>
-                {bullet}
-                <StyledListItem>{this.props.created}</StyledListItem>
-              </Typography>
-            </StyledCardSubtitle>
-          }
-          avatar={
-            <StyledAvatar src={this.props.avatar} />
-          }
-        />
-      </StyledCard>
+      <StyledLink to={`/${this.props.author}/${this.props.id}`}>
+        <StyledCard>
+          <CardMediaWrapper>
+            <StyledCardMedia image={this.props.image} title={this.props.imageAlternative} />
+          </CardMediaWrapper>
+          <CardHeader
+            title={<Typography>{this.props.title}</Typography>}
+            subheader={
+              <StyledCardSubtitle>
+                <Typography type="caption">
+                  <StyledListItem>{this.props.author}</StyledListItem>
+                  {bullet}
+                  <StyledListItem>{this.props.upvotes} Feel you</StyledListItem>
+                  {bullet}
+                  <StyledListItem>{this.props.created}</StyledListItem>
+                </Typography>
+              </StyledCardSubtitle>
+            }
+            avatar={
+              <StyledAvatar src={this.props.avatar} />
+            }
+          />
+        </StyledCard>
+      </StyledLink>
     );
   }
 }
 
 ProblemCard.propTypes = {
+  id: PropTypes.number.isRequired,
   title: PropTypes.string.isRequired,
   author: PropTypes.string.isRequired,
   avatar: PropTypes.string.isRequired,
