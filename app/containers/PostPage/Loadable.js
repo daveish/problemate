7225179d6f9309dc2ae8b269c650c9f119272d82
David Fischer <fischer@sipgate.de>
2017-10-06 14:53:03 +0200

    add chunkname for post page

--- app/containers/PostPage/Loadable.js
+++ app/containers/PostPage/Loadable.js
@@ -7,6 +7,6 @@
 import Loadable from 'react-loadable';
 
 export default Loadable({
-  loader: () => import('./index'),
+  loader: () => import(/* webpackChunkName: "post" */ './index'),
   loading: () => null,
 });
